7f15f42a46738e8593e62a5f8b5582f92230c794
orbyteon <orbyteonstudio@gmail.com>
2026-08-15 04:37:13 +0300

    Expose design buttons to runtime-built PvP UI

--- Assets/SCRIPT/Design/DesignRuntimeWiring.cs
+++ Assets/SCRIPT/Design/DesignRuntimeWiring.cs
@@ -28,6 +28,16 @@ public class DesignRuntimeWiring : MonoBehaviour
     [SerializeField] Sprite primaryButtonSprite;
     [SerializeField] Sprite secondaryButtonSprite;
 
+    public static Sprite PrimaryButtonAsset { get; private set; }
+    public static Sprite SecondaryButtonAsset { get; private set; }
+
+    void Awake()
+    {
+        // Publish the assigned assets before any runtime-built PvP UI is created.
+        PrimaryButtonAsset = primaryButtonSprite;
+        SecondaryButtonAsset = secondaryButtonSprite;
+    }
+
     void Start()
     {
         StartCoroutine(BuildNextFrame());
