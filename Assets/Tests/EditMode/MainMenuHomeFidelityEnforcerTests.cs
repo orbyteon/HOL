7898e068307ccd21e8f54ff6effa6c7d0c7989fb
orbyteon <orbyteonstudio@gmail.com>
2026-08-23 21:04:27 +0300

    test(ui): make Main Menu fidelity coverage asmdef-safe

--- Assets/Tests/EditMode/MainMenuHomeFidelityEnforcerTests.cs
+++ Assets/Tests/EditMode/MainMenuHomeFidelityEnforcerTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Events;
@@ -5,6 +7,11 @@ using UnityEngine.UI;
 
 public sealed class MainMenuHomeFidelityEnforcerTests
 {
+    const string VisualRootName = "HomeVisualRoot";
+    const string ChipName = "HomePlayerChip";
+    const string PrivateIconName = "HomePrivateIcon";
+    const string DailyIconName = "HomeDailyIcon";
+
     GameObject root;
     Canvas canvas;
 
@@ -15,43 +22,45 @@ public sealed class MainMenuHomeFidelityEnforcerTests
         canvas = root.GetComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
-        var visualRoot = Child(root.transform, MainMenuHomeVisuals.VisualRootName);
+        var visualRoot = Child(root.transform, VisualRootName);
         BuildButton(visualRoot.transform, "ButtonPlay");
         BuildButton(visualRoot.transform, "ButtonPvP");
         BuildButton(visualRoot.transform, "DailyHuntButton");
         BuildButton(visualRoot.transform, "Buttonsettings");
 
-        var chip = Child(visualRoot.transform, MainMenuHomeVisuals.ChipName);
+        var chip = Child(visualRoot.transform, ChipName);
         chip.AddComponent<Image>();
-        Child(chip.transform, "HomePlayerChipSurface").AddComponent<MainMenuPlayerChipGraphic>();
+        AddRuntimeComponent(Child(chip.transform, "HomePlayerChipSurface"),
+            "MainMenuPlayerChipGraphic");
         Child(chip.transform, "HomePlayerAvatar").AddComponent<Image>();
-        Child(chip.transform, "HomePlayerAvatarSymbol").AddComponent<MainMenuReferenceIconGraphic>();
+        AddRuntimeComponent(Child(chip.transform, "HomePlayerAvatarSymbol"),
+            "MainMenuReferenceIconGraphic");
 
-        Child(visualRoot.transform, MainMenuHomeVisuals.PrivateIconName)
-            .AddComponent<MainMenuReferenceIconGraphic>();
-        Child(visualRoot.transform, MainMenuHomeVisuals.DailyIconName)
-            .AddComponent<MainMenuReferenceIconGraphic>();
+        AddRuntimeComponent(Child(visualRoot.transform, PrivateIconName),
+            "MainMenuReferenceIconGraphic");
+        AddRuntimeComponent(Child(visualRoot.transform, DailyIconName),
+            "MainMenuReferenceIconGraphic");
 
-        Child(Find(visualRoot.transform, "Buttonsettings"), "HomeSettingsGearSymbol")
-            .AddComponent<MainMenuReferenceIconGraphic>();
+        AddRuntimeComponent(Child(Find(visualRoot.transform, "Buttonsettings"),
+            "HomeSettingsGearSymbol"), "MainMenuReferenceIconGraphic");
     }
 
     [TearDown]
     public void TearDown()
     {
-        Object.DestroyImmediate(root);
+        UnityEngine.Object.DestroyImmediate(root);
     }
 
     [Test]
     public void ApplyToCanvas_RestoresApprovedSpritesAtFullAlphaAndSlicedNineSlice()
     {
-        Assert.That(MainMenuHomeFidelityEnforcer.ApplyToCanvas(canvas), Is.True);
+        Assert.That(Apply(), Is.True);
 
         AssertNineSlice("ButtonPlay", "phase2a/hol_cta_gold_r2_9s");
         AssertNineSlice("ButtonPvP", "phase2a/hol_cta_blue_r2_9s");
         AssertNineSlice("DailyHuntButton", "phase2a/hol_cta_gold_r2_9s");
 
-        var chip = Find(root.transform, MainMenuHomeVisuals.ChipName).GetComponent<Image>();
+        var chip = Find(root.transform, ChipName).GetComponent<Image>();
         Assert.That(chip.sprite, Is.SameAs(Resources.Load<Sprite>("phase2a/hol_player_chip_r2_9s")));
         Assert.That(chip.color.a, Is.EqualTo(1f));
         Assert.That(chip.type, Is.EqualTo(Image.Type.Sliced));
@@ -69,7 +78,7 @@ public sealed class MainMenuHomeFidelityEnforcerTests
         UnityAction callback = () => calls++;
         button.onClick.AddListener(callback);
 
-        MainMenuHomeFidelityEnforcer.ApplyToCanvas(canvas);
+        Apply();
         button.onClick.Invoke();
 
         Assert.That(calls, Is.EqualTo(1));
@@ -78,24 +87,37 @@ public sealed class MainMenuHomeFidelityEnforcerTests
     [Test]
     public void ApplyToCanvas_DisablesProceduralReplacementGraphics()
     {
-        MainMenuHomeFidelityEnforcer.ApplyToCanvas(canvas);
+        Apply();
 
-        foreach (var graphic in root.GetComponentsInChildren<MainMenuReferenceIconGraphic>(true))
-            Assert.That(graphic.enabled, Is.False, graphic.name);
-        foreach (var graphic in root.GetComponentsInChildren<MainMenuPlayerChipGraphic>(true))
-            Assert.That(graphic.enabled, Is.False, graphic.name);
+        AssertRuntimeBehavioursDisabled("MainMenuReferenceIconGraphic");
+        AssertRuntimeBehavioursDisabled("MainMenuPlayerChipGraphic");
 
         var avatar = Find(root.transform, "HomePlayerAvatar").GetComponent<Image>();
         Assert.That(avatar.sprite, Is.SameAs(Resources.Load<Sprite>("reference/player_cyan_exact")));
         Assert.That(avatar.color.a, Is.EqualTo(1f));
 
-        var privateIcon = Find(root.transform, MainMenuHomeVisuals.PrivateIconName).GetComponent<Image>();
+        var privateIcon = Find(root.transform, PrivateIconName).GetComponent<Image>();
         Assert.That(privateIcon.sprite, Is.SameAs(Resources.Load<Sprite>("phase2a/hol_mode_private_r2")));
         Assert.That(privateIcon.color.a, Is.EqualTo(1f));
 
-        var dailyIcon = Find(root.transform, MainMenuHomeVisuals.DailyIconName).GetComponent<Image>();
+        var dailyIcon = Find(root.transform, DailyIconName).GetComponent<Image>();
         Assert.That(dailyIcon.sprite, Is.SameAs(Resources.Load<Sprite>("phase2a/hol_mode_daily_r2")));
         Assert.That(dailyIcon.color.a, Is.EqualTo(1f));
+    }
+
+    bool Apply()
+    {
+        var type = RuntimeType("MainMenuHomeFidelityEnforcer");
+        var method = type.GetMethod("ApplyToCanvas", BindingFlags.Public | BindingFlags.Static);
+        Assert.That(method, Is.Not.Null);
+        return (bool)method.Invoke(null, new object[] { canvas });
+    }
+
+    void AssertRuntimeBehavioursDisabled(string runtimeTypeName)
+    {
+        var type = RuntimeType(runtimeTypeName);
+        foreach (var component in root.GetComponentsInChildren(type, true))
+            Assert.That(((Behaviour)component).enabled, Is.False, component.name);
     }
 
     void AssertNineSlice(string name, string resource)
@@ -114,6 +136,18 @@ public sealed class MainMenuHomeFidelityEnforcerTests
         return go;
     }
 
+    static void AddRuntimeComponent(GameObject host, string typeName)
+    {
+        host.AddComponent(RuntimeType(typeName));
+    }
+
+    static Type RuntimeType(string name)
+    {
+        var type = Type.GetType(name + ", Assembly-CSharp");
+        Assert.That(type, Is.Not.Null, name);
+        return type;
+    }
+
     static GameObject Child(Transform parent, string name)
     {
         var go = new GameObject(name, typeof(RectTransform));
@@ -123,6 +157,7 @@ public sealed class MainMenuHomeFidelityEnforcerTests
 
     static Transform Find(Transform root, string name)
     {
+        if (root == null) return null;
         if (root.name == name) return root;
         for (int i = 0; i < root.childCount; i++)
         {
