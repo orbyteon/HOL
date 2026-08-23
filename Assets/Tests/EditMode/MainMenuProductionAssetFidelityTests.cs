77eee8b120f3f140156f3e6a98374eff2049a3bd
orbyteon <orbyteonstudio@gmail.com>
2026-08-23 19:54:28 +0300

    fix(test): access runtime Main Menu types through reflection

--- Assets/Tests/EditMode/MainMenuProductionAssetFidelityTests.cs
+++ Assets/Tests/EditMode/MainMenuProductionAssetFidelityTests.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.UI;
 
 public sealed class MainMenuProductionAssetFidelityTests
 {
+    const string ChipName = "HomePlayerChip";
+    const string TipName = "HomeTipCard";
+    const string PrivateIconName = "HomePrivateIcon";
+    const string DailyIconName = "HomeDailyIcon";
+
     GameObject root;
 
     [SetUp]
@@ -30,7 +37,7 @@ public sealed class MainMenuProductionAssetFidelityTests
         pvp.onClick.AddListener(() => calls++);
         daily.onClick.AddListener(() => calls++);
 
-        Assert.That(MainMenuProductionAssetFidelity.ApplyToRoot(root.transform), Is.True);
+        Assert.That(ApplyFidelity(), Is.True);
 
         AssertCta("ButtonPlay", "phase2a/hol_cta_gold_r2_9s");
         AssertCta("ButtonPvP", "phase2a/hol_cta_blue_r2_9s");
@@ -42,9 +49,10 @@ public sealed class MainMenuProductionAssetFidelityTests
         Assert.That(calls, Is.EqualTo(3),
             "Fidelity correction must preserve existing Button callbacks.");
 
-        Assert.That(play.GetComponent<MainMenuCtaLuminousSurface>(), Is.Null);
-        Assert.That(pvp.GetComponent<MainMenuCtaLuminousSurface>(), Is.Null);
-        Assert.That(daily.GetComponent<MainMenuCtaLuminousSurface>(), Is.Null);
+        var luminousType = RuntimeType("MainMenuCtaLuminousSurface");
+        Assert.That(play.GetComponent(luminousType), Is.Null);
+        Assert.That(pvp.GetComponent(luminousType), Is.Null);
+        Assert.That(daily.GetComponent(luminousType), Is.Null);
         Assert.That(DirectChild(play.transform, "HomeCtaInnerLight"), Is.Null);
         Assert.That(DirectChild(pvp.transform, "HomeCtaInnerLight"), Is.Null);
         Assert.That(DirectChild(daily.transform, "HomeCtaInnerLight"), Is.Null);
@@ -53,9 +61,9 @@ public sealed class MainMenuProductionAssetFidelityTests
     [Test]
     public void ApprovedChipAvatarGearIconsAndTipReplaceProceduralSubstitutes()
     {
-        Assert.That(MainMenuProductionAssetFidelity.ApplyToRoot(root.transform), Is.True);
+        Assert.That(ApplyFidelity(), Is.True);
 
-        var chip = Find(MainMenuHomeVisuals.ChipName).GetComponent<Image>();
+        var chip = Find(ChipName).GetComponent<Image>();
         AssertSprite(chip, "phase2a/hol_player_chip_r2_9s");
         Assert.That(chip.color.a, Is.EqualTo(1f));
         Assert.That(chip.type, Is.EqualTo(Image.Type.Sliced));
@@ -71,12 +79,12 @@ public sealed class MainMenuProductionAssetFidelityTests
         Assert.That(gear.color.a, Is.EqualTo(1f));
         Assert.That(DirectChild(gear.transform, "HomeSettingsGearSymbol"), Is.Null);
 
-        AssertModeIcon("ButtonPvP", MainMenuHomeVisuals.PrivateIconName,
+        AssertModeIcon("ButtonPvP", PrivateIconName,
             "phase2a/hol_mode_private_r2");
-        AssertModeIcon("DailyHuntButton", MainMenuHomeVisuals.DailyIconName,
+        AssertModeIcon("DailyHuntButton", DailyIconName,
             "phase2a/hol_mode_daily_r2");
 
-        var tip = Find(MainMenuHomeVisuals.TipName).GetComponent<Image>();
+        var tip = Find(TipName).GetComponent<Image>();
         AssertSprite(tip, "phase2a/hol_tip_frame_r2_9s");
         Assert.That(tip.color.a, Is.EqualTo(1f));
         Assert.That(tip.type, Is.EqualTo(Image.Type.Sliced));
@@ -89,31 +97,27 @@ public sealed class MainMenuProductionAssetFidelityTests
         var daily = BuildButton(parent, "DailyHuntButton", true);
         var settings = BuildButton(parent, "Buttonsettings", false);
 
-        AddProceduralIcon(pvp.transform, MainMenuHomeVisuals.PrivateIconName,
-            MainMenuReferenceIconKind.PrivateRoom);
-        AddProceduralIcon(daily.transform, MainMenuHomeVisuals.DailyIconName,
-            MainMenuReferenceIconKind.DailyHunt);
-        AddProceduralIcon(settings.transform, "HomeSettingsGearSymbol",
-            MainMenuReferenceIconKind.Gear);
+        AddProceduralIcon(pvp.transform, PrivateIconName);
+        AddProceduralIcon(daily.transform, DailyIconName);
+        AddProceduralIcon(settings.transform, "HomeSettingsGearSymbol");
 
-        var chip = NewRect(parent, MainMenuHomeVisuals.ChipName);
+        var chip = NewRect(parent, ChipName);
         var chipImage = chip.gameObject.AddComponent<Image>();
         chipImage.sprite = Resources.Load<Sprite>("phase2a/hol_player_chip_r2_9s");
         chipImage.color = new Color(1f, 1f, 1f, 0.002f);
         chipImage.type = Image.Type.Simple;
 
         var chipSurface = NewRect(chip, "HomePlayerChipSurface");
-        chipSurface.gameObject.AddComponent<MainMenuPlayerChipGraphic>();
+        chipSurface.gameObject.AddComponent(RuntimeType("MainMenuPlayerChipGraphic"));
 
         var avatar = NewRect(chip, "HomePlayerAvatar");
         var avatarImage = avatar.gameObject.AddComponent<Image>();
         avatarImage.sprite = Resources.Load<Sprite>("reference/player_cyan_exact");
         avatarImage.color = new Color(1f, 1f, 1f, 0.002f);
 
-        AddProceduralIcon(chip, "HomePlayerAvatarSymbol",
-            MainMenuReferenceIconKind.Player);
+        AddProceduralIcon(chip, "HomePlayerAvatarSymbol");
 
-        var tip = NewRect(parent, MainMenuHomeVisuals.TipName);
+        var tip = NewRect(parent, TipName);
         var tipImage = tip.gameObject.AddComponent<Image>();
         tipImage.sprite = Resources.Load<Sprite>("mainmenu/mainmenu_tip_frame_9s");
         tipImage.color = new Color(0.78f, 0.68f, 1f, 1f);
@@ -131,19 +135,17 @@ public sealed class MainMenuProductionAssetFidelityTests
 
         if (addProceduralCta)
         {
-            rect.gameObject.AddComponent<MainMenuCtaLuminousSurface>();
+            rect.gameObject.AddComponent(RuntimeType("MainMenuCtaLuminousSurface"));
             var inner = NewRect(rect, "HomeCtaInnerLight");
-            inner.gameObject.AddComponent<MainMenuChamferedCtaGraphic>();
+            inner.gameObject.AddComponent(RuntimeType("MainMenuChamferedCtaGraphic"));
         }
         return button;
     }
 
-    static void AddProceduralIcon(Transform parent, string name,
-        MainMenuReferenceIconKind kind)
+    static void AddProceduralIcon(Transform parent, string name)
     {
         var rect = NewRect(parent, name);
-        var graphic = rect.gameObject.AddComponent<MainMenuReferenceIconGraphic>();
-        graphic.Configure(kind);
+        rect.gameObject.AddComponent(RuntimeType("MainMenuReferenceIconGraphic"));
     }
 
     static RectTransform NewRect(Transform parent, string name)
@@ -152,6 +154,15 @@ public sealed class MainMenuProductionAssetFidelityTests
         var rect = (RectTransform)go.transform;
         rect.SetParent(parent, false);
         return rect;
+    }
+
+    bool ApplyFidelity()
+    {
+        var type = RuntimeType("MainMenuProductionAssetFidelity");
+        var method = type.GetMethod("ApplyToRoot",
+            BindingFlags.Public | BindingFlags.Static);
+        Assert.That(method, Is.Not.Null, "ApplyToRoot reflection contract changed.");
+        return (bool)method.Invoke(null, new object[] { root.transform });
     }
 
     void AssertCta(string buttonName, string resource)
@@ -169,7 +180,7 @@ public sealed class MainMenuProductionAssetFidelityTests
         var button = Find(buttonName);
         var icon = DirectChild(button, iconName);
         Assert.That(icon, Is.Not.Null);
-        Assert.That(icon.GetComponent<MainMenuReferenceIconGraphic>(), Is.Null,
+        Assert.That(icon.GetComponent(RuntimeType("MainMenuReferenceIconGraphic")), Is.Null,
             iconName + " must not keep the procedural replacement graphic.");
         var image = icon.GetComponent<Image>();
         Assert.That(image, Is.Not.Null);
@@ -210,5 +221,12 @@ public sealed class MainMenuProductionAssetFidelityTests
             if (found != null) return found;
         }
         return null;
+    }
+
+    static Type RuntimeType(string name)
+    {
+        var type = Type.GetType(name + ", Assembly-CSharp");
+        Assert.That(type, Is.Not.Null, name);
+        return type;
     }
 }
